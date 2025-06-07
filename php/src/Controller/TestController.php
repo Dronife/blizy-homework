dc268b1f735dcddbd8f0b380d2caa47686c10aa4
Dronife <dimassss9999@gmail.com>
2025-06-07 16:56:02 +0300

    main: Add nginx

---
+++ php/src/Controller/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/api/test', name: 'api_test', methods: ['GET'])]
+    public function test(): JsonResponse
+    {
+        return $this->json(['message' => 'API is working']);
+    }
+}
