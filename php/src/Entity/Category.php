19ac03d14feb371389a9155b7bd41025090b3aaf
Dronife <dimassss9999@gmail.com>
2025-06-14 20:39:28 +0300

    main: Add category

--- php/src/Entity/Category.php
+++ php/src/Entity/Category.php
@@ -2,7 +2,6 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Metadata\ApiResource;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\Mapping as ORM;
 
--- php/src/Entity/Model.php
+++ php/src/Entity/Model.php
@@ -2,7 +2,6 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ModelRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
